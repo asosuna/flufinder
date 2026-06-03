ecb982133a92ce0a078eec6aebca47408fe8c9c6
Osuna <asosuna@its-ctb111-01.ucsd.edu>
2026-06-02 18:21:55 -0700

    function3 inserted

--- flufinder.R
+++ flufinder.R
@@ -3,3 +3,8 @@ upload_fasta <- function(fasta_filename) {
   read.fasta(fasta_filename, seqtype = "AA", as.string = TRUE,
              set.attributes = FALSE)
 }
+
+trypsinize <- function(proteins) {
+  library(stringr)
+  lapply(proteins, str_split_1, pattern="(?<=R|K)")
+}
